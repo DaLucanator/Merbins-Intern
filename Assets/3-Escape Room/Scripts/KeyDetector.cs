dfd2c862467bd700cf97c2406f10ca18474a1524
Scott Murray <1026018@student.sae.edu.au>
2022-11-04 10:28:40 +0800

    Testing Updated Keypad scripts and Direct Interact

--- Assets/3-Escape Room/Scripts/KeyDetector.cs
+++ Assets/3-Escape Room/Scripts/KeyDetector.cs
@@ -6,16 +6,11 @@ using System;
 
 public class KeyDetector : MonoBehaviour
 {
-    public TextMeshProUGUI display;
-
-    public KeyPadControl keyPadControl;
+    
     // Start is called before the first frame update
     void Start()
     {
-        display = GameObject.FindGameObjectWithTag("Display").GetComponentInChildren<TextMeshProUGUI>();
-        display.text = "";
-
-        keyPadControl = GameObject.FindGameObjectWithTag("Keypad").GetComponent<KeyPadControl>();
+        
     }
 
     // Update is called once per frame
@@ -24,55 +19,5 @@ public class KeyDetector : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("KeypadButton"))
-        {
-            var key = other.GetComponentInChildren<TextMeshProUGUI>();
-            if (key != null)
-            {
-                var keyFeedback = other.gameObject.GetComponent<KeyFeedback>();
-
-                if (key.text == "BACK")
-                {
-                    if (display.text.Length > 0)
-                        display.text = display.text.Substring(0, display.text.Length - 1);
-                }
-                else if (key.text == "ACCESS")
-                {
-                    var accessGranted = false;
-                    bool onlyNumbers = int.TryParse(display.text, out int value);
-                    if (onlyNumbers == true && display.text.Length > 0)
-                    {
-                        accessGranted = keyPadControl.CheckIfCorrect(Convert.ToInt32(display.text));
-                    }
-
-                    if (accessGranted == true)
-                    {
-                        display.text = "ACCESS";
-                    }
-                    else
-                    {
-                        display.text = "RETRY";
-                    }
-                }
-                else if (key.text == "RESET")
-                {
-                    display.text = "";
-                }
-                else
-                {
-                    bool onlyNumbers = int.TryParse(display.text, out int value);
-                    if (onlyNumbers != true)
-                    {
-                        display.text = "";
-                    }
-
-                    if (display.text.Length < 4)
-                        display.text += key.text;
-                }
-                keyFeedback.keyHit = true;
-            }
-        }
-    }
+    
 }
--- Assets/3-Escape Room/Scripts/KeyFeedback.cs
+++ Assets/3-Escape Room/Scripts/KeyFeedback.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -8,16 +10,25 @@ public class KeyFeedback : MonoBehaviour
     public bool keyHit = false;
 
     private Color originalColor;
-    private Renderer renderer;
+    private Renderer rn;
 
     private float colorReturnTime = 0.1f;
     private float returnColor;
 
+    public TextMeshProUGUI display;
+
+    public KeyPadControl keyPadControl;
+
     // Start is called before the first frame update
     void Start()
     {
-        renderer = GetComponent<Renderer>();
-        originalColor = renderer.material.color;
+        rn = GetComponent<Renderer>();
+        originalColor = rn.material.color;
+
+        display = GameObject.FindGameObjectWithTag("Display").GetComponentInChildren<TextMeshProUGUI>();
+        display.text = "";
+
+        keyPadControl = GameObject.FindGameObjectWithTag("Keypad").GetComponent<KeyPadControl>();
     }
 
     // Update is called once per frame
@@ -26,12 +37,62 @@ public class KeyFeedback : MonoBehaviour
        if (keyHit && returnColor < Time.time)
         {
             returnColor = Time.time + colorReturnTime;
-            renderer.material.color = Color.green;
+            rn.material.color = Color.green;
             keyHit = false;
         } 
-       if (renderer.material.color != originalColor && returnColor < Time.time)
+       if (rn.material.color != originalColor && returnColor < Time.time)
         {
-            renderer.material.color = originalColor;
+            rn.material.color = originalColor;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Touch"))
+        {
+            var key = this.GetComponentInChildren<TextMeshProUGUI>();
+            if (key != null)
+            {
+                if (key.text == "BACK")
+                {
+                    if (display.text.Length > 0)
+                        display.text = display.text.Substring(0, display.text.Length - 1);
+                }
+                else if (key.text == "ACCESS")
+                {
+                    var accessGranted = false;
+                    bool onlyNumbers = int.TryParse(display.text, out int value);
+                    if (onlyNumbers == true && display.text.Length > 0)
+                    {
+                        accessGranted = keyPadControl.CheckIfCorrect(Convert.ToInt32(display.text));
+                    }
+
+                    if (accessGranted == true)
+                    {
+                        display.text = "ACCESS";
+                    }
+                    else
+                    {
+                        display.text = "RETRY";
+                    }
+                }
+                else if (key.text == "RESET")
+                {
+                    display.text = "";
+                }
+                else
+                {
+                    bool onlyNumbers = int.TryParse(display.text, out int value);
+                    if (onlyNumbers != true)
+                    {
+                        display.text = "";
+                    }
+
+                    if (display.text.Length < 4)
+                        display.text += key.text;
+                }
+                keyHit = true;
+            }
         }
     }
 
