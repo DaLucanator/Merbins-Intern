c9591a6fb0138f752959800383519d5b8a0fd15e
Scott <1026018@student.sae.edu.au>
2022-11-08 17:31:02 +0800

    Prefab for Direct Interact
    also small change to level load scripts

--- Assets/1-Base Defence/Scripts/LoadLevelTest.cs
+++ Assets/1-Base Defence/Scripts/LoadLevelTest.cs
@@ -2,12 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class LoadLevelTest : MonoBehaviour
 {
-    [SerializeField] private GlobalLevelLoader levelLoader;
+    private GlobalLevelLoader levelLoader;
 
     public void Start()
     {
-        levelLoader.LoadLevel();
+        //Finds the prefab you have put in your scene
+        levelLoader = GameObject.FindGameObjectWithTag("LevelLoader").GetComponent<GlobalLevelLoader>();
+    }
+
+    //put this on an object you want to interact with
+    //object just needs a collider on it (not trigger)
+    //then when you touch the object with the 'finger' it will trigger
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Touch"))
+        {
+            levelLoader.LoadLevel("Hub");
+        }
     }
 }
