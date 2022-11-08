f16db2327554c6f76efa6f8ea9611b4f65078ca4
Scott <1026018@student.sae.edu.au>
2022-11-08 21:51:24 +0800

    Added Hands, Teleport, Poster
    code and poster link

--- Assets/3-Escape Room/Scripts/KeyPadControl.cs
+++ Assets/3-Escape Room/Scripts/KeyPadControl.cs
@@ -5,16 +5,51 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class KeyPadControl : MonoBehaviour
 {
+    public int randNumber;
     public int correctCombination;
     public bool accessGranted = false;
 
     public GameObject glass;
     public GameObject crystal;
+
+    public GameObject poster;
+    public Material mat1;
+    public Material mat2;
+    public Material mat3;
+    public Material mat4;
+
+    private void Awake()
+    {
+        randNumber = Random.Range(1, 4);
+        
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         glass.SetActive(true);
         crystal.GetComponent<XRGrabInteractable>().enabled = false;
+
+        if (randNumber == 1)
+        {
+            poster.GetComponent<Renderer>().material = mat1;
+            correctCombination = 1511;
+        }
+        if (randNumber == 2)
+        {
+            poster.GetComponent<Renderer>().material = mat2;
+            correctCombination = 1797;
+        }
+        if (randNumber == 3)
+        {
+            poster.GetComponent<Renderer>().material = mat3;
+            correctCombination = 2495;
+        }
+        if (randNumber == 4)
+        {
+            poster.GetComponent<Renderer>().material = mat4;
+            correctCombination = 2712;
+        }
     }
 
     // Update is called once per frame
