645a7b98ebe191f8a226729cb32ade8946015278
Vanessa <van.biondo@gmail.com>
2022-11-17 12:57:08 +0800

    New Goblins animated

--- Assets/4-Arena Room/Scripts/OffSetInteractable.cs
+++ Assets/4-Arena Room/Scripts/OffSetInteractable.cs
@@ -3,7 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
-public class OffSetInteractable : MonoBehaviour
+public class OffSetInteractable : XRGrabInteractable
 {
-    
+    protected override void OnSelectEntering (SelectEnterEventArgs args)
+    {
+        base.OnSelectEntering(args);
+        MatchAttachPoint(args.interactor);
+    }
+
+    private void MatchAttachPoint(XRBaseInteractor interactor)
+    {
+        bool isDirect = interactor is XRDirectInteractor;
+        attachTransform.position = isDirect ? interactor.attachTransform.position : transform.position;
+        attachTransform.rotation = isDirect ? interactor.attachTransform.rotation : transform.rotation;
+    }
+
 }
