e713b5b96876b31697220d1846c327c7a076219e
Cody <1027166@student.sae.edu.au>
2022-11-24 03:21:55 +0800

    PreAudio

--- Assets/2-Break Room/Scripts/CollisionSFX.cs
+++ Assets/2-Break Room/Scripts/CollisionSFX.cs
@@ -4,21 +4,37 @@ using UnityEngine;
 
 
 [RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(Rigidbody))]
+
 
 public class CollisionSFX : MonoBehaviour
 {
     [SerializeField] AudioClip sfx;
     AudioSource AS;
+    float cooldown = -0.2f;
+    float activeCooldown;
 
     private void OnCollisionEnter(Collision collision)
     {
-        AS.Play();
+        if (activeCooldown >= 0)
+        {
+            AS.Play();
+            activeCooldown = cooldown;
+        }
+        
     }
 
     private void OnEnable()
     {
         AS = GetComponent<AudioSource>();
-        AS.clip = sfx;
+        if (sfx != null)
+            AS.clip = sfx;
     }
-
+    private void FixedUpdate()
+    {
+        if (activeCooldown < 0)
+        {
+            activeCooldown += Time.deltaTime;
+        }
+    }
 }
