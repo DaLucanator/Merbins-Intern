54e91a13ac65217441f80dbbe024eb39553da9be
Daniel B <1024175@student.sae.edu.au>
2022-11-24 18:18:09 +0800

    Added correct audio script
    Added a corrected audio script for falling audio, and added updated obj files to the escape room level

--- Assets/9-Global/Scripts/DBAudioEventFall.cs
+++ Assets/9-Global/Scripts/DBAudioEventFall.cs
@@ -3,24 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class DBAudioEvent : MonoBehaviour
+public class DBAudioEventFall : MonoBehaviour
 {
+    
+    //Creates a dropdown array you need to add this object within
+    //Set the function within the script component's array to "AudioSource > Play()"
     [SerializeField] private UnityEvent audioTrigger;
     public float setVelocity = 1.5f;
 
+    //Checks when this object has a collision with another object
     void OnCollisionEnter(Collision other)
     {
-        //when this object hits the floor
+        //If this object hits the floor
         if (other.gameObject.CompareTag("Floor"))
         {
-            //if this object is moving at a high enough speed
+            //And if this object is moving at a high enough speed
             if (other.relativeVelocity.magnitude > setVelocity)
             {
-            //plays an impact sound
+                //plays an impact sound
                 Debug.Log("Hit floor audio playing!");
                 audioTrigger.Invoke();
             }
-            //otherwise, doesn't play audio
+            //Otherwise, doesn't play audio
         }
     }
 }
